48b6958ec9ecae78c81b3ee3310116ddf1f5a00a
Jessica Card <jessicard@mac.com>
2020-02-06 16:33:51 -0800

    Add new boba every second

--- Assets/Scripts/BobaSpawner.cs
+++ Assets/Scripts/BobaSpawner.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class BobaSpawner : MonoBehaviour {
     public GameObject bobaPrefab;
+    public float interval = 1.0f;
+
+    float timeSinceLastBoba = 0.0f;
 
     void Start() {
-        Instantiate(bobaPrefab);
+        for (int i = 0; i < 1; i++) {
+            Instantiate(bobaPrefab);
+        }
     }
 
     void Update() {
+        timeSinceLastBoba += Time.deltaTime;
 
+        if (timeSinceLastBoba > interval) {
+            Instantiate(bobaPrefab);
+            timeSinceLastBoba = 0.0f;
+        }
     }
 }
