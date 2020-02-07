0026afef973bd409c2e3b3db817e2393451e1f21
Jessica Card <jessicard@mac.com>
2020-02-06 16:27:44 -0800

    Make BobaBrown material darker, make Boba prefab size larger, add BobaSpawner script that instantiates a single boba

---
+++ Assets/Scripts/BobaSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobaSpawner : MonoBehaviour {
+    public GameObject bobaPrefab;
+
+    void Start() {
+        Instantiate(bobaPrefab);
+    }
+
+    void Update() {
+
+    }
+}
