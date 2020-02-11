7094e2cc21de09b07dcd3bd91a16c9efe2124c14
Jessica Card <jessicard@mac.com>
2020-02-11 14:23:41 -0800

    Add cup script, move cup based on mouse position

---
+++ Assets/Scripts/CupController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log($"mouse position: {mousePosition}");
+        Debug.Log($"input mouse position: {Input.mousePosition}");
+
+        transform.position = new Vector3(
+            mousePosition.x,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
