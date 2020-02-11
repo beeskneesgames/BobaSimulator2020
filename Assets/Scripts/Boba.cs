cfdb08c377922b357201346261fb3c6227c07e28
Jessica Card <jessicard@mac.com>
2020-02-11 15:28:07 -0800

    Start new physics strategy for cup/boba interaction

---
+++ Assets/Scripts/Boba.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boba : MonoBehaviour {
+    public void FallIntoCup() {
+        Debug.Log("Here I go!");
+        // Reparent into cup
+        // Delete rigidbody
+        // Manually fall down (and inward sometimes) by updating position until
+        // it hits the bottom collider.
+    }
+}
---
+++ Assets/Scripts/BobaCatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobaCatcher : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        Boba boba = other.GetComponent<Boba>();
+
+        if (boba != null) {
+            boba.FallIntoCup();
+        }
+    }
+}
