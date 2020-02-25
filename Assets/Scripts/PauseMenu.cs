7244b981590eaf3a0d5ecfc8bd6f92d1300beb5f
Jake Card <jacob.boxer@gmail.com>
2020-02-25 14:54:21 -0800

    Add pause menu with exit button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    public void ExitGame() {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
