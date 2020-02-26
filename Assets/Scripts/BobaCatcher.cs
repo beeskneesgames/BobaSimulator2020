33fc8faa858ba98eee4243513247f3ce7370d2a5
Jessica Card <jessicard@mac.com>
2020-02-26 14:23:10 -0800

    Add boba count to debug

--- Assets/Scripts/BobaCatcher.cs
+++ Assets/Scripts/BobaCatcher.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BobaCatcher : MonoBehaviour {
+    public Text bobaCountText;
+    private int bobaCount = 0;
+    private int BobaCount {
+        get {
+            return bobaCount;
+        }
+
+        set {
+            bobaCount = value;
+            bobaCountText.text = $"Boba Count: {BobaCount}";
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
         Boba boba = other.GetComponent<Boba>();
 
         if (boba != null) {
             boba.FallIntoCup(GetComponentInParent<CupController>());
+            BobaCount++;
         }
     }
 }
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+    public Text bobaCount;
+
+    private void Start() {
+        if (GlobalVariables.isDebug) {
+            bobaCount.gameObject.SetActive(true);
+        } else {
+            bobaCount.gameObject.SetActive(false);
+        }
+    }
+}
