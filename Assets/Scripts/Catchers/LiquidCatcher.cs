7be341fe6c98ef0735312326b77603597df8ac15
Jessica Card <jessicard@mac.com>
2020-03-03 13:45:06 -0800

    Add collusion detection to liquid

---
+++ Assets/Scripts/Catchers/LiquidCatcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiquidCatcher : MonoBehaviour
+{
+    private LiquidStream currentLiquidStream;
+    public Text liquidPercentageText;
+    private float liquidPercentage = 0.0f;
+    private float LiquidPercentage {
+        get {
+            return liquidPercentage;
+        }
+
+        set {
+            liquidPercentage = value;
+            liquidPercentageText.text = $"Liquid Percentage: {LiquidPercentage * 100.0f}";
+        }
+    }
+
+    private void Update() {
+        if (currentLiquidStream != null) {
+            LiquidPercentage += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("TRIGGER ENTER LIQUID CATCHER");
+        Debug.Log($"trigger enter liquid stream other: {other}");
+        LiquidStream liquidStream = other.GetComponent<LiquidStream>();
+
+        if (liquidStream != null) {
+            currentLiquidStream = liquidStream;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        LiquidStream liquidStream = other.GetComponent<LiquidStream>();
+
+        if (liquidStream != null) {
+            currentLiquidStream = null;
+        }
+    }
+}
--- Assets/Scripts/DebuggerUI.cs
+++ Assets/Scripts/DebuggerUI.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameUI : MonoBehaviour, IDebuggerListener {
+public class DebuggerUI : MonoBehaviour, IDebuggerListener {
     public Text bobaCount;
     public Text currentPhase;
     public Text hotkeys;
+    public Text liquidPercentage;
     public Text timeRemainingText;
 
     private void Start() {
@@ -22,6 +23,7 @@ public class GameUI : MonoBehaviour, IDebuggerListener {
         bobaCount.gameObject.SetActive(Debugger.Instance.IsOn);
         currentPhase.gameObject.SetActive(Debugger.Instance.IsOn);
         hotkeys.gameObject.SetActive(Debugger.Instance.IsOn);
+        liquidPercentage.gameObject.SetActive(Debugger.Instance.IsOn);
         timeRemainingText.gameObject.SetActive(Debugger.Instance.IsOn);
     }
 }
