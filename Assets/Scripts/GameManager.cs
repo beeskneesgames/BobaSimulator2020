bcb8e43176c2176c7e944fa79eea25957d20744c
Jessica Card <jessicard@mac.com>
2020-03-07 13:40:59 -0800

    Add GameManager script that handles loading starting and exiting the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static void ExitGame() {
+        Application.Quit();
+    }
+
+    public static void StartMainScene() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public static void LoadTitleScene() {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -14,6 +14,6 @@ public class Stats : MonoBehaviour {
     }
 
     public void RestartGame() {
-        SceneManager.LoadScene("MainScene");
+        GameManager.StartMainScene();
     }
 }
