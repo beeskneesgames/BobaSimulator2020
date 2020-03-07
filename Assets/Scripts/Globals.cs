45e7af42033a8fc7909b29518fc2101d2c092ea2
Jessica Card <jessicard@mac.com>
2020-03-07 11:09:54 -0800

    Add GeneratePosition function for spawners

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals {
+    public static float GetScreenSize(Camera camera) {
+        return camera.orthographicSize * 1.5f;
+    }
+}
--- Assets/Scripts/Spawners/LiquidSpawner.cs
+++ Assets/Scripts/Spawners/LiquidSpawner.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LiquidSpawner : MonoBehaviour {
+    public float interval = 1.0f;
     public LiquidStream liquidStreamPrefab;
+
     private LiquidStream liquidStream;
+    private float timeSinceLastStream = 0.0f;
+    private float screenSize;
 
     private void Start() {
+        screenSize = Globals.GetScreenSize(Camera.main);
         liquidStream = Instantiate(liquidStreamPrefab);
+        liquidStream.transform.position = GeneratePosition();
+    }
+
+    private void Update() {
+        timeSinceLastStream += Time.deltaTime;
+
+        if (timeSinceLastStream > interval) {
+            liquidStream.transform.position = GeneratePosition();
+            timeSinceLastStream = 0.0f;
+        }
+    }
+
+    private Vector3 GeneratePosition() {
+        //float xPosition = Debugger.Instance.IsOn ? 0.0f : Random.Range(-screenSize, screenSize);
+        float xPosition = Random.Range(-screenSize, screenSize);
+        return new Vector3(xPosition, 0.0f, 0.0f);
     }
 
     private void OnDisable() {
