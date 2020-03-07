87d2ecf8c5e164271e17c61ece27bea200bddb23
Jessica Card <jessicard@mac.com>
2020-03-07 12:49:22 -0800

    Add liquid percentage and boba count to globals

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Globals {
+    public static int bobaCount = 0;
+    public static float liquidPercentage;
+
     public static float GetScreenSize(Camera camera) {
         return camera.orthographicSize * 1.5f;
+    }
+
+    public static string FormattedLiquidPercentage() {
+        return (Mathf.Min(liquidPercentage, 1.0f) * 100.0f).ToString("N0");
     }
 }
