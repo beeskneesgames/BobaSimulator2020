281c537f3d11ca0e661278a500d30c31a76d2d17
Jessica Card <jessicard@mac.com>
2020-03-07 13:35:17 -0800

    Add restart game button

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Stats : MonoBehaviour {
@@ -10,5 +11,9 @@ public class Stats : MonoBehaviour {
     private void Start() {
         bobaCountText.text = $"Boba Count: {Globals.bobaCount}";
         liquidPercentageText.text = $"Liquid Percentage: {Globals.FormattedLiquidPercentage}%";
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene("MainScene");
     }
 }
