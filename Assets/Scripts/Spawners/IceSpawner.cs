02d4417fe12df554a77d3986486707c215184bf6
Jessica Card <jessicard@mac.com>
2020-03-13 14:04:00 -0700

    Add gravity and ice script

--- Assets/Scripts/Spawners/IceSpawner.cs
+++ Assets/Scripts/Spawners/IceSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class IceSpawner : MonoBehaviour {
     public GameObject icePrefab;
-    public float interval = 1.0f;
+    public float interval = 0.5f;
 
     private float timeSinceLastIce = 0.0f;
     private float screenSize;
