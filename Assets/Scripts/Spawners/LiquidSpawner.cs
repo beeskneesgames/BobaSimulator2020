c568c3b5191e0ad8057823aceb6a186ad6c25f46
Jake Card <jacob.boxer@gmail.com>
2020-03-13 11:33:58 -0700

    Add comment explaining liquid stream z-position

--- Assets/Scripts/Spawners/LiquidSpawner.cs
+++ Assets/Scripts/Spawners/LiquidSpawner.cs
@@ -27,6 +27,11 @@ public class LiquidSpawner : MonoBehaviour {
 
     private Vector3 GeneratePosition() {
         float xPosition = Debugger.Instance.IsOn ? 0.0f : Random.Range(-screenSize, screenSize);
+
+        // Use 0.5 for the z-position here so the liquid stream appears to be
+        // going into the liquid fill. Without this, part of the stream will
+        // appear "in front of" the fill, breaking the illusion of it filling
+        // the cup.
         return new Vector3(xPosition, 0.0f, 0.5f);
     }
 
