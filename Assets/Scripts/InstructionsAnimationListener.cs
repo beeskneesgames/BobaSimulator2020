fed475e5098451a4dbebf386bd9daeb6b3e874f5
Jake Card <jacob.boxer@gmail.com>
2020-04-28 11:42:42 -0700

    Start setting up instructions animations

---
+++ Assets/Scripts/InstructionsAnimationListener.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsAnimationListener : MonoBehaviour {
+    public void BobaAnimationEnded() {
+        Debug.Log("Boba animation ended");
+    }
+}
