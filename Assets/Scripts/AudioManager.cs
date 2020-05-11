22d7750e09370c136a7a6523c00bea15feb0894c
Jessica Card <jessicard@mac.com>
2020-05-11 12:47:37 -0700

    Format audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,54 +2,46 @@ using UnityEngine.Audio;
 using System;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-{
+public class AudioManager : MonoBehaviour {
 
-	public static AudioManager instance;
+    public static AudioManager instance;
 
-	public AudioMixerGroup mixerGroup;
+    public AudioMixerGroup mixerGroup;
 
-	public Sound[] sounds;
+    public Sound[] sounds;
 
-	private void Awake()
-	{
-		if (instance != null)
-		{
-			Destroy(gameObject);
-		}
-		else
-		{
-			instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
+    private void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
-		foreach (Sound sound in sounds)
-		{
-			sound.source = gameObject.AddComponent<AudioSource>();
-			sound.source.clip = sound.clip;
-			sound.source.loop = sound.loop;
+        foreach (Sound sound in sounds) {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
 
-			sound.source.outputAudioMixerGroup = mixerGroup;
-		}
-	}
-
-    private void Start() {
-		Play("Theme");
+            sound.source.outputAudioMixerGroup = mixerGroup;
+        }
     }
 
-    public void Play(string sound)
-	{
-		Sound s = Array.Find(sounds, item => item.name == sound);
-		if (s == null)
-		{
-			Debug.LogWarning("Sound: " + sound + " not found!");
-			return;
-		}
+    private void Start() {
+        Play("Theme");
+    }
 
-		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
-		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+    public void Play(string sound) {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+        if (s == null) {
+            Debug.LogWarning("Sound: " + sound + " not found!");
+            return;
+        }
 
-		s.source.Play();
-	}
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+        s.source.Play();
+    }
 
 }
