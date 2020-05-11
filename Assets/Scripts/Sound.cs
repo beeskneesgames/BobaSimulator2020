1458b1b7d2ac5d65f10feb1671376065b91af891
Jessica Card <jessicard@mac.com>
2020-05-11 12:54:47 -0700

    Fix floats

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 [System.Serializable]
 public class Sound {
 
-	public string name;
+    public string name;
 
-	public AudioClip clip;
+    public AudioClip clip;
 
-	[Range(0f, 1f)]
-	public float volume = .75f;
-	[Range(0f, 1f)]
-	public float volumeVariance = .1f;
+    [Range(0.0f, 1.0f)]
+    public float volume = 0.75f;
+    [Range(0.0f, 1.0f)]
+    public float volumeVariance = 0.1f;
 
-	[Range(.1f, 3f)]
-	public float pitch = 1f;
-	[Range(0f, 1f)]
-	public float pitchVariance = .1f;
+    [Range(0.1f, 3.0f)]
+    public float pitch = 1.0f;
+    [Range(0.0f, 1.0f)]
+    public float pitchVariance = 0.1f;
 
-	public bool loop = false;
+    public bool loop = false;
 
-	public AudioMixerGroup mixerGroup;
+    public AudioMixerGroup mixerGroup;
 
-	[HideInInspector]
-	public AudioSource source;
+    [HideInInspector]
+    public AudioSource source;
 
 }
