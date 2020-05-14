349bde10a33b5c80571d1b1add52c7ab76df2c20
Jessica Card <jessicard@mac.com>
2020-05-14 16:34:46 -0700

    Assemblies and moving things around

---
+++ Assets/Tests/Ordering/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Order
+    {
+        [Test]
+        public void OrderScriptSimplePasses()
+        {
+        }
+    }
+}
