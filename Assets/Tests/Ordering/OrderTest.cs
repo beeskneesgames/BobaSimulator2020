bb64547b76eb51aa2cde305a047a5dae6b828c3d
Jessica Card <jessicard@mac.com>
2020-05-15 14:15:49 -0700

    Fix test name

---
+++ Assets/Tests/Ordering/OrderTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class OrderTest {
+        [Test]
+        public void OrderIsBubbleTea() {
+            Order.FlavorOption drinkType = Order.FlavorOption.Single;
+
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = drinkType,
+                drinkFlavors = Order.Flavor.Mango,
+            };
+
+            Assert.AreEqual(expected: true, actual: order.IsBubbleTea());
+        }
+
+        public void OrderNotBubbleTea() {
+            Order.FlavorOption drinkType = Order.FlavorOption.Single;
+
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.None,
+                drinkType = drinkType,
+                drinkFlavors = Order.Flavor.Mango,
+            };
+
+            Assert.AreEqual(expected: false, actual: order.IsBubbleTea());
+        }
+    }
