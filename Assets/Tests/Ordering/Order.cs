4e5260a038e6c835d6dbbcd195174ed6dcd0426f
Jessica Card <jessicard@mac.com>
2020-05-14 17:23:49 -0700

    Add test

--- Assets/Tests/Ordering/Order.cs
+++ Assets/Tests/Ordering/Order.cs
@@ -4,13 +4,32 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests
-{
-    public class Order
-    {
+namespace Tests {
+    public class Order {
         [Test]
-        public void OrderScriptSimplePasses()
-        {
+        public void OrderIsBubbleTea() {
+            Order.FlavorOption drinkType = Order.FlavorOption.Single;
+
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = drinkType,
+                drinkFlavors = Order.Flavor.Mango,
+            };
+
+            Assert.AreEqual(expected: true, actual: order.IsBubbleTea());
+        }
+
+        public void OrderNotBubbleTea() {
+            Order.FlavorOption drinkType = Order.FlavorOption.Single;
+
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.None,
+                drinkType = drinkType,
+                drinkFlavors = Order.Flavor.Mango,
+            };
+
+            Assert.AreEqual(expected: false, actual: order.IsBubbleTea());
         }
     }
-}
