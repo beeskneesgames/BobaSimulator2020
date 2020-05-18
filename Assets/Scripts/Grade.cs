ff853f780d17334149dbc36ac69e14daee1c238f
Jessica Card <jessicard@mac.com>
2020-05-18 16:30:35 -0700

    Add Grade class

---
+++ Assets/Scripts/Grade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Grade {
+
+    public Grade Compile() {
+        Grade grade = new Grade();
+
+        return grade;
+    }
+}
