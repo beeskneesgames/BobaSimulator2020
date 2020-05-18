1877016ac1a7ab58eeb5670966aff4f0678626bc
Jessica Card <jessicard@mac.com>
2020-05-18 15:58:49 -0700

    Move order generation into order class

--- Assets/Scripts/Ordering/Order.cs
+++ Assets/Scripts/Ordering/Order.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class Order {
@@ -45,15 +46,103 @@ public class Order {
         bool isValid = true;
         bool multipleFlavors = drinkFlavors.Count == 2;
 
-        if (iceAmount  == AddInOption.NotSet      ||
-            bobaAmount == AddInOption.NotSet      ||
-            drinkType  == FlavorOption.NotSet     ||
+        if (iceAmount == AddInOption.NotSet ||
+            bobaAmount == AddInOption.NotSet ||
+            drinkType == FlavorOption.NotSet ||
             drinkFlavors.First() == Flavor.NotSet ||
-            drinkFlavors.Last()  == Flavor.NotSet ||
+            drinkFlavors.Last() == Flavor.NotSet ||
             (multipleFlavors && drinkFlavors.First() == drinkFlavors.Last())) {
             isValid = false;
         }
         return isValid;
+    }
+
+    public static Order GenerateBasic() {
+        FlavorOption drinkType = FlavorOption.Single;
+
+        Order randomOrder = new Order {
+            iceAmount = AddInOption.Regular,
+            bobaAmount = AddInOption.Regular,
+            drinkType = drinkType,
+            drinkFlavors = GetRandomDrinkFlavors(drinkType),
+        };
+
+        return randomOrder;
+    }
+
+    public static Order GenerateBasic(List<Flavor> flavors) {
+        FlavorOption drinkType = FlavorOption.Single;
+
+        Order randomOrder = new Order {
+            iceAmount = AddInOption.Regular,
+            bobaAmount = AddInOption.Regular,
+            drinkType = drinkType,
+            drinkFlavors = flavors,
+        };
+
+        return randomOrder;
+    }
+
+    public static Order GenerateRandom() {
+        FlavorOption drinkType = GetRandomDrinkType();
+
+        Order randomOrder = new Order {
+            iceAmount = GetRandomOption(),
+            bobaAmount = GetRandomOption(),
+            drinkType = drinkType,
+            drinkFlavors = GetRandomDrinkFlavors(drinkType),
+        };
+
+        return randomOrder;
+    }
+
+    private static AddInOption GetRandomOption() {
+        Array addInOptions = Enum.GetValues(typeof(AddInOption));
+
+        // Start at 1 to avoid setting AddInOption.NotSet
+        AddInOption randomOption = (AddInOption)addInOptions.GetValue(
+            UnityEngine.Random.Range(1, addInOptions.Length - 1)
+        );
+
+        return randomOption;
+    }
+
+    private static FlavorOption GetRandomDrinkType() {
+        Array liquidOptions = Enum.GetValues(typeof(FlavorOption));
+
+        // Start at 1 to avoid setting FlavorOption.NotSet
+        FlavorOption randomOption = (FlavorOption)liquidOptions.GetValue(
+            UnityEngine.Random.Range(1, liquidOptions.Length - 1)
+        );
+
+        return randomOption;
+    }
+
+    private static List<Flavor> GetRandomDrinkFlavors(FlavorOption drinkType) {
+        int numberOfFlavors = (drinkType == FlavorOption.Single) ? 1 : 2;
+        List<Flavor> flavors = new List<Flavor> { };
+        Array flavorOptions = Enum.GetValues(typeof(Flavor));
+
+        // Start at 1 to avoid setting Flavor.NotSet
+        int firstIndex = UnityEngine.Random.Range(1, flavorOptions.Length - 1);
+
+        Flavor firstFlavor = (Flavor)flavorOptions.GetValue(firstIndex);
+
+        flavors.Add(firstFlavor);
+
+        if (numberOfFlavors == 2) {
+            int secondIndex;
+
+            do {
+                // Start at 1 to avoid setting Flavor.NotSet
+                secondIndex = UnityEngine.Random.Range(1, flavorOptions.Length - 1);
+            } while (firstIndex == secondIndex);
+
+            Flavor secondFlavor = (Flavor)flavorOptions.GetValue(secondIndex);
+            flavors.Add(secondFlavor);
+        }
+
+        return flavors;
     }
 
     public string ToSentence() {
