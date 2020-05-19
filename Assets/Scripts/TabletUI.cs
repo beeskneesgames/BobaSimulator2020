842579e92bc9227ddfef4484eff87cce02ce4518
Jake Card <jacob.boxer@gmail.com>
2020-05-19 15:29:46 -0700

    Set up register screen for orders

---
+++ Assets/Scripts/TabletUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabletUI : MonoBehaviour {
+    public Text orderText;
+
+    private Order displayedOrder;
+
+    private void Update() {
+        if (displayedOrder != Globals.currentOrder) {
+            displayedOrder = Globals.currentOrder;
+            orderText.text = $"<b>Order</b>\n{OrderBobaText}\n{OrderIceText}\n{OrderFlavorText}";
+        }
+    }
+
+    private string OrderBobaText {
+        get {
+            return "";
+        }
+    }
+
+    private string OrderIceText {
+        get {
+            return "";
+        }
+    }
+
+    private string OrderFlavorText {
+        get {
+            return displayedOrder.drinkFlavors[0].ToString();
+        }
+    }
+}
