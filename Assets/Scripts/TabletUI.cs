5a0adcd9acd6ac827f98f756f446c006dbd044e8
Jake Card <jacob.boxer@gmail.com>
2020-05-19 16:03:58 -0700

    Update order on ipad screen

--- Assets/Scripts/TabletUI.cs
+++ Assets/Scripts/TabletUI.cs
@@ -1,35 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class TabletUI : MonoBehaviour {
-    public Text orderText;
+    public TextMeshProUGUI orderText;
 
     private Order displayedOrder;
 
     private void Update() {
         if (displayedOrder != Globals.currentOrder) {
             displayedOrder = Globals.currentOrder;
-            orderText.text = $"<b>Order</b>\n{OrderBobaText}\n{OrderIceText}\n{OrderFlavorText}";
-        }
-    }
-
-    private string OrderBobaText {
-        get {
-            return "";
-        }
-    }
-
-    private string OrderIceText {
-        get {
-            return "";
-        }
-    }
-
-    private string OrderFlavorText {
-        get {
-            return displayedOrder.drinkFlavors[0].ToString();
+            orderText.text = $"<b>Order</b>\n{displayedOrder.FlavorDescription}\n{displayedOrder.BobaDescription}\n{displayedOrder.IceDescription}";
         }
     }
 }
