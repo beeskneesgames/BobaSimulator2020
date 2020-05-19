01e15dddf751b3ad58bc4292894b8d11bd5e8bbf
Jessica Card <jessicard@mac.com>
2020-05-19 15:19:52 -0700

    THIS IS A MESS

--- Assets/Scripts/Grade.cs
+++ Assets/Scripts/Grade.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public class Grade {
     private Dictionary<Order.AddInOption, float> perfectAddInPercentages = new Dictionary<Order.AddInOption, float> {
@@ -148,6 +148,90 @@ public class Grade {
     }
 
     public string CompileDrinkName() {
-        return "Strawberry honeydew with a splash of coconut bubble tea with light ice";
+        // This method returns a person's order as a readable sentence
+
+        // For example:
+        // Mango tea 
+        // Mango bubble tea
+        // Mango bubble tea with light ice
+        // Mango bubble tea with light boba
+        // Mango bubble tea with no ice and extra boba
+
+        // Mango blueberry bubble tea
+        // Mango with a splash of blueberry tea
+
+        return $"{CompileFlavorString()}{CompileIceString()}{CompileBobaString()}";
+    }
+
+    private string CompileIceString() {
+        string compiledString;
+        string iceAmountStr = Globals.currentOrder.iceAmount.ToString().ToLower();
+
+        switch (Globals.currentOrder.iceAmount) {
+            case Order.AddInOption.None:
+                // Mango bubble tea with no ice
+                compiledString = " with no ice";
+                break;
+            case Order.AddInOption.Regular:
+                // Mango bubble tea
+                compiledString = "";
+                break;
+            default:
+                // Mango bubble tea with light ice
+                compiledString = $" with {iceAmountStr} ice";
+                break;
+        }
+
+        return compiledString;
+    }
+
+    private string CompileBobaString() {
+        string compiledString;
+        string bobaAmountStr = Globals.currentOrder.bobaAmount.ToString().ToLower();
+
+        if (Globals.currentOrder.bobaAmount == Order.AddInOption.None || Globals.currentOrder.bobaAmount == Order.AddInOption.Regular) {
+            // Mango tea
+            compiledString = "";
+        } else if (Globals.currentOrder.iceAmount == Order.AddInOption.Regular) {
+            // Mango bubble tea with extra boba
+            compiledString = $" with {bobaAmountStr} boba";
+        } else {
+            // Mango bubble tea with no ice and light boba
+            compiledString = $" and {bobaAmountStr} boba";
+        }
+
+        return compiledString;
+    }
+
+    private string CompileFlavorString() {
+        Order.Flavor firstFlavor = Globals.currentOrder.drinkFlavors.First();
+        Order.Flavor secondFlavor = Globals.currentOrder.drinkFlavors.Last();
+        string secondFlavorStr = secondFlavor.ToString().ToLower();
+        string teaType = IsBubbleTea() ? "bubble tea" : "tea";
+
+        string compiledString;
+
+        switch (Globals.currentOrder.drinkType) {
+            case Order.FlavorOption.Splash:
+                // Mango with a splash of lemon tea
+                compiledString = $"{firstFlavor} with a splash of {secondFlavorStr} {teaType}";
+                break;
+            case Order.FlavorOption.Half:
+                // Mango lemon tea
+                compiledString = $"{firstFlavor} {secondFlavorStr} {teaType}";
+                break;
+            default:
+                // Mango tea
+                compiledString = $"{firstFlavor} {teaType}";
+                break;
+        }
+
+        return $"{compiledString}";
+    }
+
+    public bool IsBubbleTea() {
+        bool isBubbleTea = Globals.currentOrder.bobaAmount != Order.AddInOption.None && Globals.currentOrder.bobaAmount != Order.AddInOption.NotSet;
+
+        return isBubbleTea;
     }
 }
---
+++ Assets/Tests/GradeTest.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Tests {
+    public class GradeTest {
+        [Test]
+        public void OrderIsBubbleTea() {
+            Order order = new Order { bobaAmount = Order.AddInOption.Regular };
+
+            Assert.AreEqual(expected: true, actual: Order.IsBubbleTea());
+        }
+
+        [Test]
+        public void OrderNotBubbleTea() {
+            Order order = new Order();
+
+            Assert.AreEqual(expected: false, actual: order.IsBubbleTea());
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithNoIce() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.None,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea with no ice"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithLightIce() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Light,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea with light ice"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithRegularIce() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithExtraIce() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Extra,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea with extra ice"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithNoBoba() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.None,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango tea"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithLightBoba() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Light,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea with light boba"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithRegularBoba() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithExtraBoba() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Extra,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea with extra boba"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithSingleFlavor() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Single,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango, Order.Flavor.Blueberry },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango bubble tea"));
+        }
+
+
+        [Test]
+        public void CompileToSentenceStringWithHalfFlavor() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Half,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango, Order.Flavor.Blueberry },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango blueberry bubble tea"));
+        }
+
+        [Test]
+        public void CompileToSentenceStringWithSplashFlavor() {
+            Order order = new Order {
+                iceAmount = Order.AddInOption.Regular,
+                bobaAmount = Order.AddInOption.Regular,
+                drinkType = Order.FlavorOption.Splash,
+                drinkFlavors = new List<Order.Flavor> { Order.Flavor.Mango, Order.Flavor.Blueberry },
+            };
+
+            Assert.That(order.ToSentence(), Is.EqualTo("Mango with a splash of blueberry bubble tea"));
+        }
+    }
+}
