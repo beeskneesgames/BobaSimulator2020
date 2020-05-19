c6b5cc836793bec720e62c52083c3067ebb84195
Jessica Card <jessicard@mac.com>
2020-05-19 14:33:22 -0700

    Add strings

--- Assets/Scripts/Grade.cs
+++ Assets/Scripts/Grade.cs
@@ -19,18 +19,79 @@ public class Grade {
         { Order.FlavorOption.Single, 1.00f },
     };
 
+    private List<string> goodPhrase = new List<string> {
+        "Wow!",
+        "Yum!",
+        "Mmmmm.",
+    };
+
+    private List<string> mediocrePhrase = new List<string> {
+        "Hmmm.",
+        "Oh.",
+        "Well…",
+    };
+
+    private List<string> badPhrase = new List<string> {
+        "Uh oh!",
+        "Yikes!",
+        "Drats.",
+        "Bummer…",
+    };
+
+    private List<string> goodDescriptor = new List<string> {
+        "good",
+        "amazing",
+        "perfect",
+        "so yummy",
+        "refreshing",
+    };
+
+    private List<string> mediocreDescriptor = new List<string> {
+        "okay",
+        "edible",
+        "pretty good",
+        "mediocre",
+    };
+
+    private List<string> badDescriptor = new List<string> {
+        "bad",
+        "weird",
+        "upsetting",
+        "gross",
+    };
+
+    private List<string> goodComment = new List<string> {
+        "The flavor is spot on!",
+        "This is exactly what I asked for!",
+        "You should get a raise!",
+        "Thank you so much!",
+    };
+
+    // mediocre/bad comments
+    // "The flavor is a little off.",
+    // "There’s too much (splash flavor)."
+    // "There’s not enough (50/50 flavor)."
+    // "Where’s the (50/50 flavor) though?"
+    // "Do I taste (flavor that’s not supposed to be there)?"
+
+
+
     public enum LetterGrade {
         A,
         C,
         F
     }
     public LetterGrade letterGrade;
+    public string comment;
+    public string drinkName;
 
     public Grade Compile() {
         LetterGrade letterGrade = CompileLetterGrade();
 
         Grade grade = new Grade {
             letterGrade = letterGrade,
+            drinkName = CompileDrinkName(),
+            comment = CompileComment(),
         };
 
         return grade;
@@ -50,5 +111,13 @@ public class Grade {
 
     private float IcePercentage() {
         return iceCount / 100;
+    }
+
+    public string CompileComment() {
+        return "Wow! Delicious.";
+    }
+
+    public string CompileDrinkName() {
+        return "Strawberry honeydew with a splash of coconut bubble tea with light ice";
     }
 }
