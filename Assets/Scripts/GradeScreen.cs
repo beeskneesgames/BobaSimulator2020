742c67d5ea8833d85134186434aabb7d61a4768f
Jessica Card <jessicard@mac.com>
2020-05-21 15:33:42 -0700

    Update UI elements

--- Assets/Scripts/GradeScreen.cs
+++ Assets/Scripts/GradeScreen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Stats : MonoBehaviour {
+public class GradeScreen : MonoBehaviour {
     public Text bobaCountText;
     public Text liquidPercentageText;
 
