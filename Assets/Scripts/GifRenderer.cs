2e99922c59309e2d1a81d4ebe5843a69609c6b71
Jessica Card <jessicard@mac.com>
2020-05-27 13:53:40 -0700

    Add splash screen

---
+++ Assets/Scripts/GifRenderer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GifRenderer : MonoBehaviour {
+    public Sprite[] frames;
+    public Image splash;
+    private int currentImage;
+    private float frameRate;
+    private static AsyncOperation loadedTitleScene;
+
+    void Start() {
+        loadedTitleScene = SceneManager.LoadSceneAsync("TitleScene", LoadSceneMode.Single);
+        loadedTitleScene.allowSceneActivation = false;
+
+        frameRate = 0.05f;
+        currentImage = 0;
+        InvokeRepeating("ChangeImage", 0.05f, frameRate);
+    }
+
+    private void ChangeImage() {
+        if (currentImage >= frames.Length - 10) {
+            loadedTitleScene.allowSceneActivation = true;
+        }
+
+        currentImage += 1;
+        splash.sprite = frames[currentImage];
+    }
+}
