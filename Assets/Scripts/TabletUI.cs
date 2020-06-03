24ccf7635d8a3b18982c6e2c3bcfa632c44fb9e6
Jake Card <jacob.boxer@gmail.com>
2020-06-03 13:50:34 -0700

    Hook iPad UI back up to script properly

--- Assets/Scripts/TabletUI.cs
+++ Assets/Scripts/TabletUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 
 public class TabletUI : MonoBehaviour {
+    public TextMeshProUGUI orderHeader;
     public TextMeshProUGUI orderText;
     public PhaseManager phaseManager;
 
@@ -12,46 +13,40 @@ public class TabletUI : MonoBehaviour {
     private float currentTimeAnimating = 0.0f;
     private float maxTimeAnimating =  0.5f;
 
-    Transform panel;
-    Transform orderUI;
-
     private bool animating = true;
 
-    private void Start() {
-        panel = transform.Find("Panel");
-        orderUI = panel.Find("OrderUI");
-    }
-
     private void Update() {
-        if (animating) {
-            float startingPosition = orderUI.position.x;
-            float targetPosition = orderUI.position.x;
-            float fractionOfJourney = currentTimeAnimating / maxTimeAnimating;
-
-            currentTimeAnimating += Time.deltaTime;
-
-            if (maxTimeAnimating >= 0.0f && fractionOfJourney < 1.0f) {
-                orderUI.position = new Vector3(
-                    Mathf.Lerp(startingPosition, targetPosition, fractionOfJourney),
-                    orderUI.position.y,
-                    orderUI.position.z
-                );
-            } else {
-                animating = false;
-            }
-        }
-
         if (displayedOrder != Globals.currentOrder || displayedPhase != phaseManager.CurrentPhase.Name) {
             displayedOrder = Globals.currentOrder;
             displayedPhase = phaseManager.CurrentPhase.Name;
-
-            string[] lines = {
-                MarkedLine(displayedOrder.FlavorDescription.ToUpper(), displayedPhase == "Liquid Phase"),
-                MarkedLine(displayedOrder.BobaDescription.ToUpper(), displayedPhase == "Boba Phase"),
-                MarkedLine(displayedOrder.IceDescription.ToUpper(), displayedPhase == "Ice Phase")
-            };
-            orderText.text = string.Join("\n", lines);
+            UpdateDisplayedOrder();
         }
+        //if (animating) {
+        //    float startingPosition = orderUI.position.x;
+        //    float targetPosition = orderUI.position.x;
+        //    float fractionOfJourney = currentTimeAnimating / maxTimeAnimating;
+
+        //    currentTimeAnimating += Time.deltaTime;
+
+        //    if (maxTimeAnimating >= 0.0f && fractionOfJourney < 1.0f) {
+        //        orderUI.position = new Vector3(
+        //            Mathf.Lerp(startingPosition, targetPosition, fractionOfJourney),
+        //            orderUI.position.y,
+        //            orderUI.position.z
+        //        );
+        //    } else {
+        //        animating = false;
+        //    }
+        //}
+    }
+
+    private void UpdateDisplayedOrder() {
+        string[] lines = {
+            MarkedLine(displayedOrder.FlavorDescription, displayedPhase == "Liquid Phase"),
+            MarkedLine(displayedOrder.BobaDescription, displayedPhase == "Boba Phase"),
+            MarkedLine(displayedOrder.IceDescription, displayedPhase == "Ice Phase")
+        };
+        orderText.text = string.Join("\n", lines);
     }
 
     private static string MarkedLine(string line, bool isMarked) {
