fc0011cbe5fc72135cd243d03eadd17c94749844
Jessica Card <jessicard@mac.com>
2020-06-04 17:16:03 -0700

    Fix

--- Assets/Scripts/CupController.cs
+++ Assets/Scripts/CupController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CupController : MonoBehaviour {
     private void Update() {
@@ -9,9 +7,26 @@ public class CupController : MonoBehaviour {
         }
 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float screenSizeX = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        float buffer = 0.75f;
+        float rightX = cameraPos.x + screenSizeX - buffer;
+        float leftX = cameraPos.x - screenSizeX + buffer;
+        float xPosition = mousePosition.x;
+
+        if (mousePosition.x > 0) {
+            if (mousePosition.x >= rightX) {
+                xPosition = rightX;
+            }
+        } else {
+            if (mousePosition.x <= leftX) {
+                xPosition = leftX;
+            };
+        }
 
         transform.position = new Vector3(
-            mousePosition.x,
+            xPosition,
             transform.position.y,
             transform.position.z
         );
