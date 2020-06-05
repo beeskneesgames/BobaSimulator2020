9c14c8de5817c2b6182e9cc0e3dce5bd9452f7da
Jessica Card <jessicard@mac.com>
2020-06-04 17:42:15 -0700

    Address comments

--- Assets/Scripts/CupController.cs
+++ Assets/Scripts/CupController.cs
@@ -1,29 +1,26 @@
 ﻿using UnityEngine;
 
 public class CupController : MonoBehaviour {
+    float screenSizeX;
+    float buffer = 0.75f;
+    float rightX;
+    float leftX;
+    Vector3 cameraPos;
+
+    private void Start() {
+        cameraPos = Camera.main.transform.position;
+        screenSizeX = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        rightX = cameraPos.x + screenSizeX - buffer;
+        leftX = cameraPos.x - screenSizeX + buffer;
+    }
+
     private void Update() {
         if (Globals.isPaused) {
             return;
         }
 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float screenSizeX = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
-        Vector3 cameraPos = Camera.main.transform.position;
-
-        float buffer = 0.75f;
-        float rightX = cameraPos.x + screenSizeX - buffer;
-        float leftX = cameraPos.x - screenSizeX + buffer;
-        float xPosition = mousePosition.x;
-
-        if (mousePosition.x > 0) {
-            if (mousePosition.x >= rightX) {
-                xPosition = rightX;
-            }
-        } else {
-            if (mousePosition.x <= leftX) {
-                xPosition = leftX;
-            };
-        }
+        float xPosition = Mathf.Clamp(mousePosition.x, leftX, rightX);
 
         transform.position = new Vector3(
             xPosition,
