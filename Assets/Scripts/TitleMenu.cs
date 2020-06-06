dd9dba5e83b16257fa9841eb71afb0585533d4be
Jake Card <jacob.boxer@gmail.com>
2020-06-06 16:07:32 -0700

    Fade in title and main scenes

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TitleMenu : MonoBehaviour {
+    public SceneFader sceneFader;
+
     private void Start() {
         Debugger.Instance.IsOn = false;
     }
 
     public void StartGame() {
-        GameManager.StartMainScene();
+        sceneFader.FadeOut(() => {
+            GameManager.StartMainScene();
+        });
     }
 
     public void ShowCredits() {
