e468b8c5a618b4b96dc72ab8838697828af9071a
Jake Card <jacob.boxer@gmail.com>
2020-06-06 18:15:58 -0700

    Add random straw colors

--- Assets/Scripts/CupContainer.cs
+++ Assets/Scripts/CupContainer.cs
@@ -6,15 +6,24 @@ public class CupContainer : MonoBehaviour {
     public GameObject lid;
     public GameObject straw;
 
+    private readonly Color[] strawColors = {
+        new Color(1.0f,   1.0f,   1.0f  ), // #FFFFFF white
+        new Color(0.259f, 0.871f, 1.0f  ), // #42DEFF blue
+        new Color(0.992f, 1.0f,   0.359f), // #FDFF5B yellow
+    };
+
     private void Start() {
         DontDestroyOnLoad(gameObject);
         Globals.cupContainer = this;
     }
 
     public void PrepareForGradeScreen() {
+        // Show the lid
+        lid.SetActive(true);
+
         // Show the straw and pick a color for it
         straw.SetActive(true);
-        lid.SetActive(true);
+        straw.GetComponent<Renderer>().material.color = strawColors[Random.Range(0, strawColors.Length)];
 
         // Make cup more transparent
         Renderer cupRenderer = cup.GetComponent<Renderer>();
